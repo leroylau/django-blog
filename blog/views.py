b186a0fa56c38cbaa3cda31a55c1ede1c734fdec
william <luroylau@163.com>
2018-08-29 10:51:06 +0800

    Roll bacl to the version without blueprint design.

---
+++ blog/views.py
@@ -0,0 +1,45 @@
+from flask import render_template, request, flash
+
+from blog import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user')
+def user():
+    name = 'william'
+    posts = [
+        {
+            'name' : 'william',
+            'body' : "A lovely day."
+        },
+        {
+            'name' : 'william',
+            "body" : 'A trip to Japan.'
+        }
+    ]
+    return render_template('user.html', name=name, posts=posts)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/register/regist', methods=['GET', 'POST'])
+def regist():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['pwd']
+    
+    error = None
+    if not username:
+        error = 'Please enter your name.'
+    if not email:
+        error = 'Please enter your email.'
+    if not password:
+        error = 'Please enter your password.'
+
+    flash(error)
+
+    return f"welcome {username}!"
--- flaskr/auth/auth.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__, url_prefix='/auth')
-
