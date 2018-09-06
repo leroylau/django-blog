5339b33d0f940446ce2b332e225ebf269180b907
william <luroylau@163.com>
2018-09-06 10:18:28 +0800

    实现了使用session来保持用户登录信息，实现了本地存储用户注册信息和blog内容

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, request, flash, redirect, url_for
+import json, os
+
+from flask import flash, redirect, render_template, request, session, url_for
 
 from blog import app
+
 
 @app.route('/')
 def index():
@@ -11,24 +14,19 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = int(request.form.get('pwd'))
-        print(username, password)
-    if username == '111' and password == 222:
-        return redirect(url_for('user'))
+        with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/user.json", 'r') as f:
+            users = json.load(f)
+            if (username in users) and (password == users[username]):
+                session['username'] = username
+                return redirect(url_for('blog'))
     return render_template('login.html')
 
 @app.route('/blog')
-def user():
-    user = {'name' : 'william'}
-    posts = [
-        {
-            'name' : 'william',
-            'body' : "A lovely day."
-        },
-        {
-            'name' : 'william',
-            "body" : 'A trip to Japan.'
-        }
-    ]
+def blog():
+    user = {'name' : session['username']}
+    with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/blog.json", 'r') as f:
+        blogs = json.load(f)
+        posts = blogs[session['username']]
     return render_template('blog.html', user=user, posts=posts)
 
 @app.route('/register')
