6b00caa4bf8f398918fea22948cc7db12a9aefa1
william <luroylau@163.com>
2018-09-06 15:07:46 +0800

    增加logout功能

--- blog/views.py
+++ blog/views.py
@@ -21,33 +21,48 @@ def login():
                 return redirect(url_for('blog'))
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 @app.route('/blog')
 def blog():
-    user = {'name' : session['username']}
-    with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/blog.json", 'r') as f:
-        blogs = json.load(f)
-        posts = blogs[session['username']]
-    return render_template('blog.html', user=user, posts=posts)
+    if 'username' in session:
+        user = {'name' : session['username']}
+        with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/blog.json", 'r') as f:
+            blogs = json.load(f)
+            posts = blogs[session['username']]
+        return render_template('blog.html', user=user, posts=posts)
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
-
-@app.route('/register/regist', methods=['GET', 'POST'])
-def regist():
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['pwd']
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = int(request.form.get('pwd'))
     
-    error = None
-    if not username:
-        error = 'Please enter your name.'
-    if not email:
-        error = 'Please enter your email.'
-    if not password:
-        error = 'Please enter your password.'
+        error = None
+        if not username:
+            error = 'Please enter your name.'
+        if not email:
+            error = 'Please enter your email.'
+        if not password:
+            error = 'Please enter your password.'
 
-    flash(error)
+        flash(error)
 
-    return f"welcome, {username}!"
+        with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/user.json", 'w+') as f:
+            users = json.load(f)
+            users[username] = password
+            json.dump(users, f)
+            flash("注册成功！")
+        with open("/Users/liuchang/Documents/Projects/WebProgrammingWithPythonAndjavaScript/projects/flask-tutorial" + "/blog.json", 'w+') as f:
+            blogs = json.load(f)
+            blogs[username] = []
+            json.dump(blogs, f)
+            return redirect(url_for('login'))
+    return render_template('register.html')
